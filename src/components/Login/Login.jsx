b88ab42c77ed34032cc7cfdf7f1b77b095567a33
Gonzalo Manzano <gonzalo040903@gmail.com>
2023-08-25 17:45:41 -0300

    fix: arreglo de login y logica

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,28 +4,27 @@ import { password } from "../Login/validar";
 import { user } from "../Login/validar";
 import { useNavigate } from "react-router-dom";
 
-
 const Login = () => {
   const URL_BASE = import.meta.env.VITE_API_URL_BASE;
 
   const navigate = useNavigate();
   async function onSubmit(data) {
-    console.log(data)
+    console.log(data);
     try {
-      const res = await fetch(URL_BASE + '/login', {
+      const res = await fetch(URL_BASE + "/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
-      const {token} = await res.json()
+      const { token } = await res.json();
       if (token) {
-        localStorage.setItem('token', token)
-        navigate('/admin')
-      } 
+        localStorage.setItem("token", token);
+        navigate("/admin");
+      }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
   const {
@@ -37,7 +36,7 @@ const Login = () => {
     <div className="row" id="login">
       <div className="col-3 d-flex justify-content-center" id="inicioSesion">
         <form onSubmit={handleSubmit(onSubmit)} className="formcontrolinicio">
-          <h2 className="text-center textoiniciosesion">Inicio Sesion</h2>
+          <h2 className="text-center textoiniciosesion">INICIO SESION</h2>
           <div className="contenedorDeInput container text-center">
             <label htmlFor="user">Usuario</label>
             <input
@@ -51,16 +50,7 @@ const Login = () => {
                 validate: user,
               })}
             />
-            {errors.user?.type === "required" && (
-              <p>Ingrese su Usuario. Por favor. </p>
-            )}
-            {errors.user && <p> Usuario Incorrecto.</p>}
-            {errors.user?.type === "minLength" && (
-              <p>Ingrese como minimo 5 caracteres. Por favor. </p>
-            )}
-            {errors.user?.type === "maxLength" && (
-              <p>Ingresó el maximo de caracteres. </p>
-            )}
+            {errors.user && <p className="errorEnLogin"> Usuario Incorrecto.</p>}
           </div>
           <div className="contenedorDeInput container mt-2 text-center">
             <label htmlFor="password">Contraseña</label>
@@ -76,16 +66,7 @@ const Login = () => {
                 validate: password,
               })}
             />
-            {errors.password?.type === "required" && (
-              <p>Ingrese su Contraseña. Por favor. </p>
-            )}
-            {errors.password && <p>Contraseña Incorrecta. Prueba otra vez. </p>}
-            {errors.password?.type === "minLength" && (
-              <p>Ingrese como minimo 5 caracteres. Por favor. </p>
-            )}
-            {errors.password?.type === "maxLength" && (
-              <p>Ingresó el maximo de caracteres para una password. </p>
-            )}
+            {errors.password && <p className="errorEnLogin">Contraseña Incorrecta. Prueba otra vez. </p>}
           </div>
           <button
             // onClick={handleLogin}
